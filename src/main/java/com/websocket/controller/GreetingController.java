a8ff4afb5359ebee97ecf9a954f13c8140144c4f
root <admin@example.com>
2020-09-03 23:46:38 +0200

    first commit 03/09/20

---
+++ src/main/java/com/websocket/controller/GreetingController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.websocket.controller;
+
+/**
+ *
+ * @author marco
+ */
+import com.websocket.model.Greeting;
+import com.websocket.model.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+
+    
+    /** Il payload del messaggio è associato a un oggetto HelloMessage, che viene passato a greeting ().
+     * @param message
+     * @return 
+     * @throws java.lang.Exception **/ 
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings")
+  public Greeting greeting(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+}
---
+++ src/main/java/com/websocket/model/Greeting.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.websocket.model;
+
+/**
+ *
+ * @author marco
+ */
+
+public class Greeting {
+
+  private String content;
+
+  public Greeting() {
+  }
+
+  public Greeting(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+}
